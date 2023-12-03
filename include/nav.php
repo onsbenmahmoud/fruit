1bbf3ded5ef47629051c4906ba77dff26c89a052
onsbenmahmoud <152807372+onsbenmahmoud@users.noreply.github.com>
2023-12-03 20:01:21 +0100

    Add files via upload

---
+++ include/nav.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>complete responsive fruits website design</title>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    />
+    <link rel="stylesheet" href="projett.css" />
+  </head>
+  <body>
+    <header>
+      <a href="#" class="logo">fruits<span>.</span></a>
+      <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#products">products</a>
+        <a href="#contact">contact</a>
+      </nav>
+      <div class="icons">
+        <a href="#" class="fas fa-heart"></a>
+        <a href="#" class="fas fa-shopping-cart"></a>
+        <a href="#" class="fas fa-user"></a>
+      </div>
+    </header>
+</body>
+</html>
